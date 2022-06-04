3ddfbc812de0f90290647f1d1397b54d7646159d
ishanshre <ishan.shrestha356@gmail.com>
2022-06-04 10:37:52 +0545

    Create super user

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Publisher, Contributor, Book, Review, BookContributor
 # Register your models here.
+admin.site.register(Publisher)
+admin.site.register(Contributor)
+admin.site.register(Book)
+admin.site.register(Review)
+admin.site.register(BookContributor)
